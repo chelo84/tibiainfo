a6ba45f06ba10ae8d1103377ab2f20d89bc0b6bb
marcoslacroix <marcoslacroix@gmail.com>
2020-05-18 19:24:36 -0300

    Initial commit

---
+++ src/test/java/com/tibiainfo/TibiainfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibiainfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiainfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
