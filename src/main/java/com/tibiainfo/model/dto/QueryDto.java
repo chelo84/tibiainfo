2e1caff0c382adb761eb7c7d186cc0b68c971adc
marcoslacroix <marcoslacroix@gmail.com>
2020-05-19 18:06:13 -0300

    Added item endpoint

---
+++ src/main/java/com/tibiainfo/model/dto/QueryDto.java
@@ -0,0 +1,14 @@
+package com.tibiainfo.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QueryDto {
+
+    Integer page;
+
+    Integer size;
+
+}
---
+++ src/main/java/com/tibiainfo/model/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.tibiainfo.model.repository;
+
+import com.tibiainfo.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
+
+
+}
