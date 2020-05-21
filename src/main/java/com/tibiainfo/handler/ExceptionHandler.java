3ffb216a534dd82fc2a0cfd5c83d344b6cd61cb1
Chelo84 <marcelo_lacroix@hotmail.com>
2020-05-20 21:43:04 -0300

    Now gets the request's URI instead of the ContextPath

--- src/main/java/com/tibiainfo/handler/ExceptionHandler.java
+++ src/main/java/com/tibiainfo/handler/ExceptionHandler.java
@@ -1,13 +1,14 @@
 package com.tibiainfo.handler;
 
+import com.tibiainfo.exception.NotFoundException;
 import com.tibiainfo.model.dto.ErrorDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -15,8 +16,19 @@ import java.time.LocalDateTime;
 @RestController
 public class ExceptionHandler {
 
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    public final ResponseEntity<Object> handleNotFoundException(Exception ex, HttpServletRequest request) {
+        ErrorDTO errorDetails = buildErrorDetails(
+                ex,
+                request,
+                HttpStatus.NOT_FOUND
+        );
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleExceptions(Exception ex, WebRequest request) {
+    public final ResponseEntity<Object> handleExceptions(Exception ex, HttpServletRequest request) {
         ErrorDTO errorDetails = buildErrorDetails(
                 ex,
                 request,
@@ -26,14 +38,14 @@ public class ExceptionHandler {
         return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private ErrorDTO buildErrorDetails(Exception ex, WebRequest request, HttpStatus httpStatus) {
+    private ErrorDTO buildErrorDetails(Exception ex, HttpServletRequest request, HttpStatus httpStatus) {
         log.error(ex.getMessage(), ex);
         return ErrorDTO.builder()
                 .timestamp(LocalDateTime.now())
                 .status(httpStatus.value())
                 .error(httpStatus.getReasonPhrase())
                 .message(ex.getMessage())
-                .path(request.getContextPath())
+                .path(request.getRequestURI())
                 .build();
     }
 
