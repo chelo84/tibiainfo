87cc7460580a696efb33bc3dcf327fee574382d1
mlacroix <mlacroix@tdsoft.com.br>
2020-05-25 19:09:07 -0300

    install pip

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Tibia Info API
 
 Documentation can be found at `http://localhost:8080/swagger-ui.html` locally.
+
+Be aware that this program may install python pip to your computer.
---
+++ src/main/java/com/tibiainfo/exception/PipInstallationException.java
@@ -0,0 +1,9 @@
+package com.tibiainfo.exception;
+
+public class PipInstallationException extends Exception {
+
+    public PipInstallationException(String message) {
+        super(message);
+    }
+
+}
